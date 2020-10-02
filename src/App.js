d062ea0f24d3cb5de11dda73a28ee93430b6a9f6
Shail <raval.shail1@gmail.com>
2020-10-02 14:22:09 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeComponent from './Components/HomeComponent.jsx'
+function App() {
+  return (
+    <div className="App">
+      <HomeComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HomeComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class HomeComponent extends Component{
+    render(){
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+export default HomeComponent;

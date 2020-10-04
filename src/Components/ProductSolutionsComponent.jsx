235e768f67f6413efb1232b7a8dd698a253c3e0a
briskweb2519 <briskweb2519@gmail.com>
2020-10-04 14:29:56 +0530

    'commit'

--- src/Components/ProductSolutionsComponent.jsx
+++ src/Components/ProductSolutionsComponent.jsx
@@ -2,11 +2,73 @@ import React, { Component } from 'react'
 import Image from 'react-bootstrap/Image'
 import img from '../Images/69.jpg'
 
-class ProductSolutionsComponent extends Component{
-    render(){
-        return(
+import { Container } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
+// import CardDeck from 'react-bootstrap/CardDeck'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+class ProductSolutionsComponent extends Component {
+    render() {
+        return (
             <>
-                <Image src={img} fluid />
+                <Container>
+                    <Image src={img} fluid className="mb-3" />
+                    <Row>
+                        <Col xs={12} md={6} className="mb-3">
+                            <Card className="frame">
+                                <Card.Img variant="top" src={img} />
+                                <Card.Body class="details">
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} md={6} className="mb-3">
+                            <Card className="frame">
+                                <Card.Img variant="top" src={img} />
+                                <Card.Body class="details">
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={12} md={6} className="mb-3">
+                            <Card className="frame">
+                                <Card.Img variant="top" src={img} />
+                                <Card.Body class="details">
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} md={6} className="mb-3">
+                            <Card className="frame">
+                                <Card.Img variant="top" src={img} />
+                                <Card.Body class="details">
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+
+                </Container>
             </>
         )
     }
